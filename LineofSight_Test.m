2a25622708d8c34bfa35ed347ea05c45a02d98ed
Bo Xin <bxin@lsst.org>
2016-01-14 12:31:48 -0700

    add test#3

--- LineofSight_Test.m
+++ LineofSight_Test.m
@@ -10,15 +10,36 @@ if iTest==1
     % vx = [1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 ]';
 
 %% test input #2
+% a system-wide tilt around x-axis of TCRS.
 elseif iTest==2
     dM1M2 = 6628-472; % =6156, in mm (zemax has 6156.2006)
     dM1Cam = 5336-1938; % = 3398 in mm (zemax has 3398.6)
     dM1M3 = -233.8; %in mm
-    dM1EL = -1895+5425;
+    dM1AZ = -1895+5425;
+    dM2AZ = dM1AZ + dM1M2;
+    dM3AZ = dM1AZ + dM1M3;
+    dCamAZ = dM1AZ + dM1Cam;
+    
+    angleAS = 1; %angle in arcsec
+    angleDEG = angleAS/3600;
+    angleRad= angleDEG/180*pi; %angle in radian
+    M1Motion = [-dM1AZ*(1-cos(angleRad))*1000 0 -dM1AZ*sin(angleRad)*1000 angleAS 0 0];
+    M2Motion = [-dM2AZ*(1-cos(angleRad))*1000 0 -dM2AZ*sin(angleRad)*1000 angleAS 0 0];
+    M3Motion = [-dM3AZ*(1-cos(angleRad))*1000 0 -dM3AZ*sin(angleRad)*1000 angleAS 0 0];
+    CamMotion = [-dCamAZ*(1-cos(angleRad))*1000 0 -dCamAZ*sin(angleRad)*1000 angleAS 0 0];
+    vx = [M1Motion M2Motion M3Motion CamMotion]';
+    
+elseif iTest==3
+    
+    % a system-wide tilt around elevation-axis.
+    
+    dM1M2 = 6628-472; % =6156, in mm (zemax has 6156.2006)
+    dM1Cam = 5336-1938; % = 3398 in mm (zemax has 3398.6)
+    dM1M3 = -233.8; %in mm
+    dM1EL = -1895;
     dM2EL = dM1EL + dM1M2;
     dM3EL = dM1EL + dM1M3;
     dCamEL = dM1EL + dM1Cam;
-    
     
     angleAS = 1; %angle in arcsec
     angleDEG = angleAS/3600;
